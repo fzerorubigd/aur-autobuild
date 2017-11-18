e4fa7272ceceba6a63855d2b71b32b6fc61dbb65
fzerorubigd <fzerorubigd@gmail.com>
2017-11-18 17:11:04 +0330

    disable ssh host key check

--- deploy-rsync.sh
+++ deploy-rsync.sh
@@ -3,4 +3,4 @@
 eval $(ssh-agent)
 ssh-add deploy_key
 
-rsync --recursive -e "ssh -p ${SSH_PORT}" repo/ ${SSH_USER}@${SSH_HOST}:${SSH_PATH}
+rsync --recursive -e "ssh -p ${SSH_PORT} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" repo/ ${SSH_USER}@${SSH_HOST}:${SSH_PATH}
